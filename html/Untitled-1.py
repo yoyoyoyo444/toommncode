0c59702a4577d606c0a8d7bc7e5feb87a59c41fd
yoyoyoyo444 <yoyoyoyyoyoyoy141@gmail.com>
2025-08-16 16:10:01 +0700

    Add files via upload

---
+++ html/Untitled-1.py
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <h1>
+        ถังซาน
+    </h1>
+    <input type="text" placeholder="ชื่อนามสกุล"> 
+    <input type="texr" placeholder="เบอร์">
+    <input type="text" placeholder="รหัสจากข้อความ">
+    <br>
+    <br>
+    <button><a href="https://anime-indy.com/douluo-dalu-soul-land-thai/">คลิกเพื่อไปหน้าเว็บถังซาน</a></button>
+
+    
+    
